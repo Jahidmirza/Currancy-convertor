5a11fa26bca69c3d258ebdef9796c011e35dc6c7
Jahid Mirza <Jahidmirza22@gmail.com>
2021-10-13 09:33:37 +0600

    ok

---
+++ app.js
@@ -0,0 +1,12 @@
+const dropList = document.querySelector('.drop-list select');
+
+for (let i = 0; i < dropList.length; i++) {
+    for (currency_code in country_code){
+
+        console.log(currency_code)
+        // let optionTag = 
+        // `<option value="${currency_code}">${currency_code}</option>`;
+
+        // dropList[i].insertAdjactionHTML('befoeend', optionTag)
+    }
+}
